41e046eb35596dccb17c13bc1ff69cbdc2e90bf2
wally <wally@DESKTOP-LKJC7SU>
2017-10-11 14:18:36 -0500

    Test last one

--- src/model/Api.java
+++ src/model/Api.java
@@ -11,7 +11,6 @@ public class Api {
 	 */
 	public static String validateUser(String username, String password){
 		//if user is valid, return a new User("User info")
-		String allBack = "";
 		return null;
 	}
 	
