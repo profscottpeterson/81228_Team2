56c0d44e35a8cc7ea28394b8a34d1fe7e1211cd6
wally <wally@DESKTOP-LKJC7SU>
2017-10-11 14:14:39 -0500

    This is a test to see if it works.

--- src/model/Api.java
+++ src/model/Api.java
@@ -11,7 +11,7 @@ public class Api {
 	 */
 	public static String validateUser(String username, String password){
 		//if user is valid, return a new User("User info")
-		
+		String allBack = "";
 		return null;
 	}
 	
