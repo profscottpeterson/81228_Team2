f1e7146912de801a757b160ce46c95b14420a72b
Perrishnikov <perry@192.168.2.1>
2017-10-10 22:59:51 -0500

    Added comments and cleaned up the code a little.

--- src/model/Api.java
+++ src/model/Api.java
@@ -2,12 +2,23 @@ package model;
 
 public class Api {
 
+	/**
+	 * API: returns a csv String with all User info if valid, else returns null
+	 * ex: "John, Doe, 123 Main St., Green Bay, WI, ..."
+	 * @param username String
+	 * @param password String
+	 * @return String || null
+	 */
 	public static String validateUser(String username, String password){
 		//if user is valid, return a new User("User info")
 		
 		return null;
 	}
 	
+	/**
+	 * API: returns <type>?> with all the MenuItems
+	 * @returns <type?>
+	 */
 	public static String getAllMenuItems() {
 		return null;
 	}
--- src/model/PizzaPlanet.java
+++ src/model/PizzaPlanet.java
@@ -1,47 +1,50 @@
 package model;
 
-import view.Quinn;
-
-import java.util.List;
-
-import model.*;
-
 public class PizzaPlanet {
-	//	Quinn view;
 	String name;
 	String address;
 
 	Menu menu;
-	User user;
-	MenuItem m;
+	User user; 
+	MenuItem m; //placeholder
 
 	public PizzaPlanet() {
 
-		
+		// Start the app off by demanding that view display this screen...
 		view.PizzaPlanetGui.userMenu(null);
 
-		System.out.println("This is Pizza Planet constructor, reporting for duty.");
 	}
 
+	/**
+	 * Called from the View to handle the (Guest, Login) option from the first screen
+	 * If Strings are passed in, demand API call database to see if User exists
+	 * @param usernameIn String; Username passed in by User in GUI
+	 * @param passwordIn String; Password passed in by User in GUI
+	 */
 	public void handleUserClick(String usernameIn, String passwordIn) {
-		//If this is called, check the db for username and password
-		String username = "";
-		String password = "";
+		// TODO: should prolly do some initial validation before calling db
+		String username = ""; 
+		String password = ""; 
 
-		if (usernameIn == ""  && passwordIn == "")
+		if (username == ""  && password == "")
 		{
+			//If this User is a GUEST, use controller below to call next view
 			callCategoryScreen();
-		}
-
-		else {
+		} 
+		else 
+		{
+			//If there is a valid User, return it, else returns null
 			String ValidUser = Api.validateUser(username, password);
 
 			if (ValidUser == null) 
 			{
+				//If User is not valid, loop back to the first screen with error message
 				view.PizzaPlanetGui.userMenu("Invalid User");
 			}
 			else
 			{
+				// If User is valid, create a new User (in state), 
+				// then use controller below to call next view
 				user = new User(ValidUser);
 				callCategoryScreen();
 			}
@@ -49,16 +52,25 @@ public class PizzaPlanet {
 	}
 
 	/*
-	 * API call: Get ALL menuItems
-	 * create a list using the catagory from each item
-	 * creatin a new Menu from that catagory
+	 * Controller: demand that View display the menuCats screen
+	 * API: Get ALL menuItems, pass that info to the view
+	 * Create a list using the catagory from each item
 	 */
 	private void callCategoryScreen() {
-		// TODO Auto-generated method stub
-		String menu = Api.getAllMenuItems();
-		view.PizzaPlanetGui.menuCats(menu, this.user);
+		//TODO: should prolly parse this info before sending it to the view.
+		// could set the state with all the menuItems as a cache so we dont  
+		// need to make more calls in the future.
+		String menu = Api.getAllMenuItems();  
 		
+		view.PizzaPlanetGui.menuCats(menu, this.user);
 	}
 
+	/**
+	 * Called from View Category screen...
+	 * @param category String; Type of category User selected
+	 */
+	public void handleCatMenuClick(String category) {
+		//Do something
+	}
 
 }
