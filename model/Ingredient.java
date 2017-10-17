4eb98a24cb8b8b6bc9d20b42d72c80db78401caf
hwillgen <32106606+hwillgen@users.noreply.github.com>
2017-10-17 17:07:21 -0500

    Add files via upload

---
+++ model/Ingredient.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Ingredient {
+
+	private String name;
+	
+	private int category;
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getCategory()
+	{
+		return category;
+	}
+	
+}
