4f86af3eaa496a8a45b97a473d94f54ca2a81109
Perry Sieber <perrishnikov@gmail.com>
2017-10-24 18:45:33 -0500

    manual update

--- src/model/PizzaPlanet.java
+++ src/model/PizzaPlanet.java
@@ -5,18 +5,18 @@ import view.Quinn;
 import model.*;
 
 public class PizzaPlanet {
-	PizzaPlanetGui frame;
+	PizzaPlanetGui view;
 	String name;
 	String address;
 
 	Menu menu;
-	User user;
+	User user = null;
 
 	public PizzaPlanet() {
-		frame = new PizzaPlanetGui();
-//		this.user = getThisUser();
+		view = new PizzaPlanetGui();
+		this.displyUserPage();
+		
 		this.menu = getMenu();
-		frame.getUserPage();
 		
 		System.out.println("This is Pizza Planet constructor, reporting for duty.");
 	}
@@ -26,42 +26,56 @@ public class PizzaPlanet {
 		return null;
 	}
 
-	/*
-	 * USER: getType
-	 * @return Guest or Login 
-	 */
-/*	private User getThisUser() {
-		User user;
-		String userSelect = view.getUserType();
-
-		switch (userSelect) {
-		case "GUEST":
-			user = new User("GUEST");
-			break;
-		case "LOGIN":
-			user = validateUser();
-			break;
-		default: 
-			user = null;
-		}
-		
-		if(user != null) {
-			return user;
-		}
-		else {
-			return null;
-		}
-	}*/
-
+	
 	/*
 	 * USER: get
 	 * validate the Users username and password
 	 * if its valid, return that User
 	 * else, return null
 	 */
-	public static User validateUser() {
-		Quinn.userLogin();
+	public User apiValidateUser(String[] creds) {
+		
+		/*we have a GUEST */
+		if (creds == null) {
+			this.displayFirstMenu();
+		} 
+		/*we have a USER */
+		else {
+			User returnedUser = Api.validateUser(creds);
+			
+			/* Invalid User or Password*/
+			if(returnedUser == null) {
+				this.displyUserPage(); //TODO: pass in error
+			}
+			/* Valid User, go to next page*/
+			else {
+				setUser(returnedUser);
+				this.displayFirstMenu();	
+			}
+		}
+		
 		return null;
 	}
+	
+	public User getUser() {
+		return this.user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	/* Call to view - User Page*/
+	private void displyUserPage() {
+		view.UserPage(this.user, this);
+	}
+	
+	/* Call to view - First Menu Page*/
+	private void displayFirstMenu() {
+		//Menu cats = Api.GetFoodTypes();
+		view.get1stMenuPage(this.getUser(), null, this);
+	}
+
+
 
 }
