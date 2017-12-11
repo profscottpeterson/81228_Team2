06a57e30ef03f62b2837c385e0f33826d1feb7fc
andrewmoule <32113208+andrewmoule@users.noreply.github.com>
2017-12-10 18:09:30 -0600

    Update Payment info

--- src/model/Payment.java
+++ src/model/Payment.java
@@ -1,5 +1,19 @@
 package model;
 
-public class Payment {
-
+public class Payment extends Api {
+	public void createOrder(int userID,double price) {
+		price = price * 1.05;
+		rs = GetResultSet("insert into Orders(userID,date,orderTotal) values ('" + userID + "',GETDATE()," + price + ")");
+	}
+	public void insertOrderInDB(Order o) {
+		for (MenuItem m : o.getOrderItems()) {
+			rs = GetResultSet("insert into Order_Food(foodID,price ) Values (" + m.getItemIngred() + "," + m.getPrice() + ")");
+			double price = 0;
+			price = price + m.getPrice();
+			
+			//createOrder(pp.user.Id, price);
+		}
+		
+	}
+	
 }
